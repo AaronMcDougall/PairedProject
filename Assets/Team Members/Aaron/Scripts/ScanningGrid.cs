ee88fc9d55d3288064b1adb3b10a5178e70c0954
AaronMcDougall <1021572@student.sae.edu.au>
2021-10-16 14:15:33 +1100

    World object scanner working

--- Assets/Team Members/Aaron/Scripts/ScanningGrid.cs
+++ Assets/Team Members/Aaron/Scripts/ScanningGrid.cs
@@ -8,7 +8,7 @@ using Color = UnityEngine.Color;
 public class ScanningGrid : MonoBehaviour
 {
     public LayerMask layer;
-    public Vector2 worldSize;
+    public Vector2Int worldSize;
 
     private Node[,] grid;
 
@@ -20,45 +20,53 @@ public class ScanningGrid : MonoBehaviour
     {
         public bool isBlocked;
     }
-
+    
     private void Start()
     {
+        //how many grid spots in the world
         gridSizeX = Mathf.RoundToInt(worldSize.x / nodeSize);
         gridSizeY = Mathf.RoundToInt(worldSize.y / nodeSize);
+        
+        grid = new Node [worldSize.x, worldSize.y] ;
         CreateGrid();
     }
 
     void CreateGrid()
     {
-        grid = new Node[gridSizeX, gridSizeY];
-        
         for (int x = 0; x < gridSizeX; x++)
         {
-
             for (int y = 0; y < gridSizeY; y++)
             {
-
-                bool isBlocked = !(Physics.CheckBox(new Vector3(x, y, 0), 
-                    new Vector2(nodeSize, nodeSize), Quaternion.identity, layer));
-                
                 grid[x, y] = new Node();
-                
-                Debug.Log(grid);
-
+                if ((Physics.CheckBox(new Vector3(x, y, 0),
+                    new Vector2(nodeSize, nodeSize), Quaternion.identity, layer)))
+                {
+                    grid[x, y].isBlocked = true;
+                }
             }
         }
     }
+    
     private void OnDrawGizmos()
     {
         //grid parameters outline
         Gizmos.DrawWireCube(transform.position, new Vector3(worldSize.x, worldSize.y, -1));
 
         //drawing and filling grid
-        for (int x = 0; x < worldSize.x; x++)
+        for (int x = 0; x < gridSizeX; x++)
         {
-            for (int y = 0; y < worldSize.y; y++) {
-
-                    Gizmos.DrawCube(new Vector3(x, y, -1), Vector3.one);
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                if (grid != null && grid[x, y].isBlocked)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawCube(new Vector3(x, y, 0), Vector3.one);
+                }
+                else
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawCube(new Vector3(x, y, 0), Vector3.one);
+                }
             }
         }
     }
