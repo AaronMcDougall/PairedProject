2cbeb7c0cfaa6312576ff99ab1c4fc80a465e6f1
lphillips17 <lj.phillips@hotmail.com>
2021-10-17 15:18:05 +1100

    Leaving State In Progress
    Testing...

--- Assets/Scripts/Patron States/LeavingState.cs
+++ Assets/Scripts/Patron States/LeavingState.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using NodeCanvas.Tasks.Actions;
 using UnityEngine;
 
 public class LeavingState : StateBase
 {
+    //the location of moving to the exit for the patrons
+    private GoToPoint exit;
+    public GameObject destinationLoc;
+    public Vector3 destination;
+
+    //move speed
+    public float speed=10.0f;
+    
     public override void Enter()
     {
         base.Enter();
@@ -14,6 +23,21 @@ public class LeavingState : StateBase
         base.Execute();
     }
 
+    private void LeaveArea()
+    {
+        destinationLoc = GameObject.FindGameObjectWithTag("Exit");
+        destination = destinationLoc.transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, destination, (speed * Time.deltaTime));
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Exit"))
+        {
+            Destroy(GameObject.FindWithTag("Patron"));
+        }
+    }
+    
     public override void Exit()
     {
         base.Exit();
